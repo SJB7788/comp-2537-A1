fb91d4ba6a153dd4b35dbf59d359be6b337f7d04
SeungJae Baek <sbj7788@gmail.com>
2023-05-01 12:14:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
+        saveUninitialized: false,
+        resave: true,
         cookie: {
             maxAge: expireTime,
         },
