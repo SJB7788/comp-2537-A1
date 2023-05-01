fff9d2956a5da919331785ac977e376ff93330ba
SeungJae Baek <sbj7788@gmail.com>
2023-05-01 11:39:22 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ main().catch(error => console.log(error));
 async function main() {
     try {
         await mongoose.connect(`mongodb+srv://${process.env.MONGOOSE_USER}:${process.env.MONGOOSE_PASSWORD}@cluster0.0c1wpzp.mongodb.net/${process.env.MONGOOSE_FOLDER}?retryWrites=true&w=majority`)
+        console.log("connected to DB");
     } catch (error) {
         console.log(error);
         process.exit(1);
