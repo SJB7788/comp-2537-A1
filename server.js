843b6ca00910375b75410eddb43a84436276aa68
SeungJae Baek <sbj7788@gmail.com>
2023-05-08 19:45:13 -0700

    asd

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ main().catch(error => console.log(error));
 async function main() {
     await mongoose.connect(`mongodb+srv://${process.env.MONGOOSE_USER}:${process.env.MONGOOSE_PASSWORD}@cluster0.0c1wpzp.mongodb.net/comp2537A2?retryWrites=true&w=majority`)
 
-    app.listen(3000, () => {
+    app.listen(process.env.PORT||3000, () => {
         console.log("running on port 3000")
     })
 }
